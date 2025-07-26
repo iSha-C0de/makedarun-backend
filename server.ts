0b47e113f7ea25229253b93bb38fcace72eccb0c
Vernyll Jan Asis <asisvjp.nvsu@gmail.com>
2025-07-26 22:23:06 +0800

    Fix: use process.env.PORT for Render deployment

--- server.ts
+++ server.ts
@@ -7,10 +7,14 @@
 //   console.log(`🚀 Server running on http://192.168.100.127:${PORT}`);
 // });
 
-import mongoose from 'mongoose';
+import express from 'express';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI!)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB error", err));
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
